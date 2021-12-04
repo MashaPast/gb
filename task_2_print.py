a8f8a9a5341f8112102a2a982a225638832deff3
Masha <mariya.pastushenkova@akbarsdigital.ru>
2021-12-04 17:03:47 +0300

    added tasks lesson 4

---
+++ task_2_print.py
@@ -0,0 +1,4 @@
+from task_2 import currency_rates
+
+print(f"USD: {currency_rates('usd')}")
+print(f"EUR: {currency_rates('EUR')}")
---
+++ utils.py
@@ -0,0 +1,5 @@
+from task_2 import currency_rates
+
+for i in range(0, 5):
+    val = currency_rates('EUR')
+    print(val)
